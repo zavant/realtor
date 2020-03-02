1c176b065a95d9265d17cb3671ca50c5ae6dd3a0
Zach <zach.avant89@gmail.com>
2020-03-01 22:50:32 -0600

    lecture 46

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Realtor
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
-    list_display_links = ('id', 'name')
+    list_display = ('id', 'name', 'email', 'hire_date', 'is_mvp')
+    list_display_links = ('id', 'name', 'is_mvp')
     search_fields = ('name',)
     list_per_page = 25
 
