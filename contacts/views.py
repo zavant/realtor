54d8595ba4dcd4cdfbe87f5e042067dee217cfd4
Zach <zach.avant89@gmail.com>
2020-03-26 22:27:33 -0500

    Changes 3/26

--- contacts/views.py
+++ contacts/views.py
@@ -29,13 +29,13 @@ def contact(request):
 
         #Send mail
 
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been a new inquiry for ' + listing + '. Sign into the admin panel for more info',
-            'slayvant@gmail.com',
-           ['slayvant@hotmail.com'],
-           fail_silently=False
-        )
+        #send_mail(
+        #    'Property Listing Inquiry',
+        #    'There has been a new inquiry for ' + listing + '. Sign into the admin panel for more info',
+        #    'slayvant@gmail.com',
+        #   ['slayvant@hotmail.com'],
+        #   fail_silently=False
+       # )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
